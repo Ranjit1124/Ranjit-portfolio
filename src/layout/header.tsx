8f5bcb8b5ee696f1fe0a512a187252a794b8a921
Ranjit1124 <ranjitpr1124@gmail.com>
2025-01-27 17:34:25 +0530

    update(2)-27/01/2025

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,7 +1,6 @@
 import { Link as ScrollLink } from 'react-scroll';
 import { Link } from 'react-router-dom';
 import { menuList } from "@utils/fackData/menuList"
-import { Meteor } from "@magic-ui/react";
 type Props = {
     isFixed: boolean
 }
