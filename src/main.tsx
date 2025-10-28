5664673b3a814879512bcda8f976d62e29c4be54
rp1 <ranjith.pollachi@cadt-solutions.com>
2025-10-28 17:38:57 +0530

    lint-fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import { HelmetProvider } from 'react-helmet-async'   // ✅ Add this
+import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min'
 import './index.css'
 import App from './App.tsx'
 
+// Wrap App inside HelmetProvider
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </StrictMode>
 )
